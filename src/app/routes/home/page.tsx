b968d6461d9cf9b08a9edf4d4ec00c5512a904aa
walid lhaila <walidlhaila00@gmail.com>
2024-12-27 23:23:18 +0100

    SIRH-8 Feat: ADD SEARCH INPUT IN THE HOME PAGE FOR JOBS OFFERS

--- src/app/routes/home/page.tsx
+++ src/app/routes/home/page.tsx
@@ -4,6 +4,7 @@ import JobCard from "@/app/components/UI/jobCard";
 import Navbar from "@/app/components/navbar";
 import {useAppDispatch, useAppSelector} from "@/lib/frontend/redux/hooks";
 import {getAllJobs} from "@/lib/frontend/redux/slices/jobSlice";
+import Search from "@/app/components/UI/search";
 
 function Page() {
     const dispatch = useAppDispatch();
@@ -43,9 +44,16 @@ function Page() {
                                 <p className="mt-2 text-lg/8 text-gray-600">Learn how to grow your business with our
                                     expert advice.</p>
                             </div>
-                            <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+                            <div>
+                                <Search />
+                            </div>
+                            <div
+                                className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
                                 {jobs.map((job) => (
-                                    <JobCard key={job._id} title={job.title} location="Casablanca" type="Full Time" company="Cegidim" description="Responsible for developing and maintaining web applications." createdBy="Walid Lhaila" />
+                                    <JobCard key={job._id} title={job.title} location="Casablanca" type="Full Time"
+                                             company="Cegidim"
+                                             description="Responsible for developing and maintaining web applications."
+                                             createdBy="Walid Lhaila"/>
                                 ))}
                             </div>
                         </div>
