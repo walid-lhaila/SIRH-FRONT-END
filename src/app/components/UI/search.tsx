48340ad0f3e93ac442793515fc61878b1f3d6f41
walid lhaila <walidlhaila00@gmail.com>
2024-12-27 23:41:16 +0100

    SIRH-9 Feat: SEARCH LOGIC BY TITLE IMPLEMENTATION FOR JOBS OFFERS

--- src/app/components/UI/search.tsx
+++ src/app/components/UI/search.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-function Search() {
+function Search({ onSearch }) {
+    const [searchTerm, setSearchTerm] = useState('');
+    const handlSearch = (e: React.FormEvent) => {
+        e.preventDefault();
+        onSearch(searchTerm);
+    }
     return (
-        <form className="max-w-md mx-auto pt-10">
+        <form className="max-w-md mx-auto pt-10" onSubmit={handlSearch}>
             <label htmlFor="default-search"
                    className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
             <div className="relative">
@@ -15,7 +20,7 @@ function Search() {
                               d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                     </svg>
                 </div>
-                <input type="search" id="default-search"
+                <input type="search" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}
                        className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-100 rounded-lg shadow-xl shadow-sky-100 focus:ring-blue-500 focus:border-blue-500 dark:border-gray-200 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
                        placeholder="Search ..." required/>
                 <button type="submit"
