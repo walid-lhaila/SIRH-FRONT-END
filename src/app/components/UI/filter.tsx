16616786079d05cd8bb92df12a5daf0957d6da29
walid lhaila <walidlhaila00@gmail.com>
2024-12-28 14:46:47 +0100

    SIRH-55 Feat: IMPLEMENT FILTRED JOBS OFFERS BY CONTRACT TYPE AND LOCATION

--- src/app/components/UI/filter.tsx
+++ src/app/components/UI/filter.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import UseGetAllCities from "@/app/hooks/useGetAllCities";
 
-function Filter() {
+function Filter({ onLocationChange, onTypeChange }) {
         const { cities } = UseGetAllCities();
-        console.log(cities);
-
     return (
         <form className="max-w-sm w-[50%] flex items-center gap-4 pt-10">
 
             <label htmlFor="states" className="sr-only tetx-center">Location</label>
-            <select id="states"
+            <select id="states" onChange={(e) => onLocationChange(e.target.value)}
                     className="block w-full p-4 text-center text-sm text-gray-900 border bg-white border-gray-100 rounded-lg shadow-xl shadow-sky-100 focus:ring-blue-500 focus:border-blue-500 dark:border-gray-200 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 <option selected>Location</option>
                 {cities.map((city) => (
@@ -18,11 +16,11 @@ function Filter() {
             </select>
 
             <label htmlFor="states" className="sr-only">Contract type</label>
-            <select id="states"
+            <select id="states" onChange={(e) => onTypeChange(e.target.value)}
                     className="block w-full p-4  text-center text-sm text-gray-900 border bg-white border-gray-100 rounded-lg shadow-xl shadow-sky-100 focus:ring-blue-500 focus:border-blue-500 dark:border-gray-200 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 <option selected>Contract type</option>
-                <option value="Full-time">Full-time</option>
-                <option value="Part-time">Part-time</option>
+                <option value="Full time">Full time</option>
+                <option value="Part time">Part time</option>
                 <option value="Internship">Internship</option>
                 <option value="Freelance">Freelance</option>
                 <option value="Consulting">Consulting</option>
