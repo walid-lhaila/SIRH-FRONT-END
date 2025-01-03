68501f2d364ec67de2a9c0ae6e6e50cb86e81ca3
walid lhaila <walidlhaila00@gmail.com>
2025-01-03 10:57:03 +0100

    CI/CD

--- src/lib/backend/controllers/applicationsController.ts
+++ src/lib/backend/controllers/applicationsController.ts
@@ -2,15 +2,54 @@ import dbConnect from "@/lib/backend/database/database";
 import { ApplicationsService } from "@/lib/backend/services/applicationsService";
 import jwt from 'jsonwebtoken';
 
+interface DecodedToken {
+    userId: string;
+}
+
 export const ApplicationsController = {
     async apply(formData: FormData, token: string) {
         try {
             await dbConnect();
 
             const secretKey = process.env.JWT_SECRET;
-            const decoded = jwt.verify(token, secretKey) as {userId: string}
+            const decoded = jwt.verify(token, secretKey);
 
-            if(!decoded || !decoded.userId){
+            // Type guard to check if decoded is of type DecodedToken
+            if (typeof decoded === 'object' && decoded !== null && 'userId' in decoded) {
+                const userId = (decoded as DecodedToken).userId;
+
+                const title = formData.get('title') as string;
+                const description = formData.get('description') as string;
+                const location = formData.get('location') as string;
+                const type = formData.get('type') as string;
+                const company = formData.get('company') as string;
+                const status = formData.get('status') as 'pending' | 'accepted' | 'rejected';
+                const createdBy = formData.get('createdBy') as string;
+                const cv = formData.get('cv') as File;
+
+                if (!title || !description || !location || !type || !company || !status || !createdBy || !cv) {
+                    return {
+                        status: 400,
+                        data: {
+                            success: false,
+                            message: 'All Fields Are Required',
+                        },
+                    };
+                }
+
+                const cvBuffer = await cv.arrayBuffer();
+
+                const createApply = await ApplicationsService.apply({ title, company, createdBy, cv: Buffer.from(cvBuffer), description, location, status, type, user: userId });
+
+                return {
+                    status: 201,
+                    data: {
+                        success: true,
+                        message: 'Application Created Successfully',
+                        data: createApply,
+                    },
+                };
+            } else {
                 return {
                     status: 401,
                     data: {
@@ -19,39 +58,6 @@ export const ApplicationsController = {
                     },
                 };
             }
-
-            const title = formData.get('title') as string;
-            const description = formData.get('description') as string;
-            const location = formData.get('location') as string;
-            const type = formData.get('type') as string;
-            const company = formData.get('company') as string;
-            const status = formData.get('status') as 'pending' | 'accepted' | 'rejected';
-            const createdBy = formData.get('createdBy') as string;
-            const user = decoded.userId;
-            const cv = formData.get('cv') as File;
-
-            if (!title || !description || !location || !type || !company || !status || !createdBy || !cv) {
-                return {
-                    status: 400,
-                    data: {
-                        success: false,
-                        message: 'All Fields Are Required',
-                    },
-                };
-            }
-
-            const cvBuffer = await cv.arrayBuffer();
-
-            const createApply = await ApplicationsService.apply({ title,  company,  createdBy,  cv: Buffer.from(cvBuffer),  description,  location,  status,  type,  user });
-
-            return {
-                status: 201,
-                data: {
-                    success: true,
-                    message: 'Application Created Successfully',
-                    data: createApply,
-                },
-            };
         } catch (error) {
             console.error('Application Failed:', error);
             return {
@@ -64,13 +70,26 @@ export const ApplicationsController = {
         }
     },
 
-    async getApplicationByUserId (token: string) {
+    async getApplicationByUserId(token: string) {
         try {
             await dbConnect();
             const secretKey = process.env.JWT_SECRET;
-            const decoded = jwt.verify(token, secretKey) as { userId: string };
+            const decoded = jwt.verify(token, secretKey);
 
-            if (!decoded || !decoded.userId){
+            // Type guard to check if decoded is of type DecodedToken
+            if (typeof decoded === 'object' && decoded !== null && 'userId' in decoded) {
+                const userId = (decoded as DecodedToken).userId;
+
+                const applications = await ApplicationsService.getApplicationByUserId(userId);
+                return {
+                    status: 200,
+                    data: {
+                        success: true,
+                        message: 'Application Retrieved Successfully',
+                        data: applications,
+                    },
+                };
+            } else {
                 return {
                     status: 401,
                     data: {
@@ -79,17 +98,8 @@ export const ApplicationsController = {
                     },
                 };
             }
-            const applications = await ApplicationsService.getApplicationByUserId(decoded.userId);
-            return {
-                status: 200,
-                data: {
-                    success: true,
-                    message: 'Application Retrieved Successfully',
-                    data: applications,
-                },
-            };
         } catch (error) {
-            console.error('Failed To Retrieved Applications:', error);
+            console.error('Failed To Retrieve Applications:', error);
             return {
                 status: 500,
                 data: {
@@ -100,4 +110,3 @@ export const ApplicationsController = {
         }
     }
 };
-
